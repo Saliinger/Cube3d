238263bf9c18401e94c60b1c20d09c677b12d76e
Jonathan <peliium.2022@gmail.com>
2025-01-23 15:27:25 +0100

    parsing

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: jalbiser <jalbiser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 22:32:16 by jalbiser          #+#    #+#             */
-/*   Updated: 2025/01/18 19:20:47 by jalbiser         ###   ########.fr       */
+/*   Updated: 2025/01/21 02:38:50 by jalbiser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -192,12 +192,13 @@ static bool is_empty_line(const char *line)
     return (true);
 }
 
-static int verify_data(t_data *data)
+static int verify_data(t_data *data, int i)
 {
     if (!data->no_texture || !data->so_texture || !data->we_texture || 
         !data->ea_texture || !data->floor || !data->ceiling)
     {
-        printf("Error: Missing required configuration elements\n");
+        if (i)
+            printf("Error: Missing required configuration elements\n");
         return (0);
     }
     return (1);
@@ -277,7 +278,7 @@ int parser(char *file, t_data *data)
         }
         if (!found && !is_empty_line(line))  // Fonction à ajouter pour vérifier les lignes vides
         {
-			if (ft_strrchr("01", line[0]))
+			if (ft_strrchr("01", line[0]) && verify_data(data, 0))
 			{
 				if (dup_line[ft_strlen(dup_line) - 1] == '\n')
 					data->map[z] = ft_strndup(dup_line, (ft_strlen(dup_line) - 1));
@@ -287,7 +288,7 @@ int parser(char *file, t_data *data)
 			}
 			else
 			{
-				printf("Error: Invalid identifier in line: %s\n", line);
+                printf("Error: problem with the file");
 				free(data->map);
 				data->map = NULL;
 				free(line);
@@ -301,7 +302,7 @@ int parser(char *file, t_data *data)
     }
 	data->map[z] = NULL;
     close(fd);
-    return (verify_data(data));  // Fonction à ajouter pour vérifier que toutes les données sont présentes
+    return (verify_data(data, 1));  // Fonction à ajouter pour vérifier que toutes les données sont présentes
 }
 
 int check_adjacent_spaces(t_data *data, int i, int j)
@@ -446,36 +447,36 @@ t_data	parsing(char **args)
     return (result);
 }
 
-//int main(int argc, char **argv)
-//{
-//    t_data data;
-//
-//    if (argc != 2)
-//    {
-//        printf("Usage: %s <path_to_cub_file>\n", argv[0]);
-//        return (1);
-//    }
-//
-//    data = parsing(argv);
-//
-//    printf("North Texture: %s\n", data.no_texture);
-//    printf("South Texture: %s\n", data.so_texture);
-//    printf("West Texture: %s\n", data.we_texture);
-//    printf("East Texture: %s\n", data.ea_texture);
-//
-//    printf("Floor color: [%d, %d, %d]\n", data.floor[0], data.floor[1], data.floor[2]);
-//    printf("Ceiling color: [%d, %d, %d]\n", data.ceiling[0], data.ceiling[1], data.ceiling[2]);
-//
-//	if (data.map)
-//    {
-//        printf("Map:\n");
-//        for (int i = 0; data.map[i]; i++)
-//        {
-//            printf("%s\n", data.map[i]);
-//        }
-//    }
-//
-//    free_data(&data);
-//
-//    return (0);
-//}
+int main(int argc, char **argv)
+{
+   t_data data;
+
+   if (argc != 2)
+   {
+       printf("Usage: %s <path_to_cub_file>\n", argv[0]);
+       return (1);
+   }
+
+   data = parsing(argv);
+
+   printf("North Texture: %s\n", data.no_texture);
+   printf("South Texture: %s\n", data.so_texture);
+   printf("West Texture: %s\n", data.we_texture);
+   printf("East Texture: %s\n", data.ea_texture);
+
+   printf("Floor color: [%d, %d, %d]\n", data.floor[0], data.floor[1], data.floor[2]);
+   printf("Ceiling color: [%d, %d, %d]\n", data.ceiling[0], data.ceiling[1], data.ceiling[2]);
+
+	if (data.map)
+   {
+       printf("Map:\n");
+       for (int i = 0; data.map[i]; i++)
+       {
+           printf("%s\n", data.map[i]);
+       }
+   }
+
+   free_data(&data);
+
+   return (0);
+}
