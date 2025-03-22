e59294615c870c5dca0d1ca8b009bce0e0bd6c7b
Saliinger <alexisnoukan@gmail.com>
2025-03-22 02:24:49 +0100

    intersection calcule fini

--- src/exec/raycasting/intersection.c
+++ src/exec/raycasting/intersection.c
@@ -4,13 +4,13 @@ static void init_delta(t_game *game, float *delta_x, float *delta_y, char flag)
 {
   if (flag == 'x')
   {
-    *delta_x = floor(game->player->y / TILE_SIZE) * TILE_SIZE;
-    *delta_y = game->player->x + (*delta_x - game->player->y) / tan(game->rayon->angle);
+    *delta_x = floor(game->player->x / TILE_SIZE) * TILE_SIZE;
+    *delta_y = game->player->y + (*delta_x - game->player->x) / tan(game->rayon->angle);
   }
   if (flag == 'y')
   {
-    *delta_x = floor(game->player->x / TILE_SIZE) * TILE_SIZE;
-    *delta_y = game->player->y + (*delta_x - game->player->x) / tan(game->rayon->angle);
+    *delta_x = floor(game->player->y / TILE_SIZE) * TILE_SIZE;
+    *delta_y = game->player->x + (*delta_x - game->player->y) / tan(game->rayon->angle);
   }
 }
 
@@ -22,15 +22,24 @@ float inter_x(t_game *game, t_rayon *ray)
   float step_y;
 
   init_delta(game, &delta_x, &delta_y, 'x');
+<<<<<<< HEAD
   step_x = TILE_SIZE / tan(ray->angle);
   step_y = TILE_SIZE;
   while (wall_hit(game->window->map, delta_x, delta_y))
+=======
+  step_x = TILE_SIZE;
+  step_y = TILE_SIZE / tan(ray->angle);
+  while(wall_hit(game, game->window->map, step_x, step_y))
+>>>>>>> 625534bd5667920def00baa02f7300752d0fe39c
   {
     delta_x += step_x;
     delta_y += step_y;
   }
+<<<<<<< HEAD
   ray->x_x = delta_x;
   ray->x_y = delta_y;
+=======
+>>>>>>> 625534bd5667920def00baa02f7300752d0fe39c
   return (sqrt(pow(delta_x - game->player->x, 2) + pow(delta_y - game->player->y, 2)));
 }
 
@@ -42,15 +51,25 @@ float inter_y(t_game *game, t_rayon *ray)
   float step_y;
 
   init_delta(game, &delta_x, &delta_y, 'y');
+<<<<<<< HEAD
   step_x = TILE_SIZE;
   step_y = TILE_SIZE / tan(ray->angle);
   while (wall_hit(game->window->map, delta_x, delta_y))
+=======
+  step_x = TILE_SIZE / tan(ray->angle);
+  step_y = TILE_SIZE;
+
+  while(wall_hit(game, game->window->map, step_x, step_y))
+>>>>>>> 625534bd5667920def00baa02f7300752d0fe39c
   {
     delta_x += step_x;
     delta_y += step_y;
   }
+<<<<<<< HEAD
   ray->y_x = delta_x;
   ray->y_y = delta_y;
+=======
+>>>>>>> 625534bd5667920def00baa02f7300752d0fe39c
   return (sqrt(pow(delta_x - game->player->x, 2) + pow(delta_y - game->player->y, 2)));
 }
 
