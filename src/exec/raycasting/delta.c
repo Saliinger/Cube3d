c5d8df76ed4651da0ef59990b26212f7be17887f
Saliinger <alexisnoukan@gmail.com>
2025-03-31 09:25:14 +0200

    done

--- src/exec/raycasting/delta.c
+++ src/exec/raycasting/delta.c
@@ -6,48 +6,44 @@
 /*   By: anoukan <anoukan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/30 19:31:54 by anoukan           #+#    #+#             */
-/*   Updated: 2025/03/31 06:34:19 by anoukan          ###   ########.fr       */
+/*   Updated: 2025/03/31 08:17:05 by anoukan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../include/exec.h"
 
-// X_AXIS
-float	delta_x_x(t_game *game, float delta_y)
+float delta_x_x(t_game *game, float delta_y)
 {
-	float	delta_x;
+    float delta_x;
 
-	delta_x = game->player->x + (delta_y - game->player->y)
-		/ tan(game->rayon->angle);
-	return (delta_x);
+    delta_x = game->player->x + (delta_y - game->player->y) / tan(game->rayon->angle);
+    return (delta_x);
 }
 
-float	delta_x_y(t_game *game)
+float delta_x_y(t_game *game)
 {
-	float	delta_y;
+    float delta_y;
 
-	delta_y = floor(game->player->y / TILE_SIZE) * TILE_SIZE;
-	if (game->rayon->angle > 0 && game->rayon->angle < M_PI)
-		delta_y += TILE_SIZE;
-	return (delta_y);
+    delta_y = floor(game->player->y / TILE_SIZE) * TILE_SIZE;
+    if (game->rayon->angle > 0 && game->rayon->angle < M_PI) 
+        delta_y += TILE_SIZE;
+    return (delta_y);
 }
 
-// Y_AXIS
-float	delta_y_x(t_game *game)
+float delta_y_x(t_game *game)
 {
-	float	delta_x;
+    float delta_x;
 
-	delta_x = floor(game->player->x / TILE_SIZE) * TILE_SIZE;
-	if (game->rayon->angle > M_PI / 2 && game->rayon->angle < 3 * M_PI / 2)
-		delta_x -= 1;                                                      
-	return (delta_x);
+    delta_x = floor(game->player->x / TILE_SIZE) * TILE_SIZE;
+    if (!(game->rayon->angle > M_PI / 2 && game->rayon->angle < 3 * M_PI / 2)) 
+        delta_x += TILE_SIZE;
+    return (delta_x);
 }
 
-float	delta_y_y(t_game *game, float delta_x)
+float delta_y_y(t_game *game, float delta_x)
 {
-	float delta_y;
+    float delta_y;
 
-	delta_y = game->player->y + (delta_x - game->player->x)
-		* tan(game->rayon->angle);
-	return (delta_y);
+    delta_y = game->player->y + (delta_x - game->player->x) * tan(game->rayon->angle);
+    return (delta_y);
 }
--- src/exec/raycasting/step.c
+++ src/exec/raycasting/step.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: anoukan <anoukan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/30 19:44:03 by anoukan           #+#    #+#             */
-/*   Updated: 2025/03/31 06:24:43 by anoukan          ###   ########.fr       */
+/*   Created: 2025/03/31 07:02:16 by anoukan           #+#    #+#             */
+/*   Updated: 2025/03/31 08:59:05 by anoukan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,10 @@ float	step_x_x(t_game *game)
 	float	step;
 
 	step = TILE_SIZE / tan(game->rayon->angle);
-	if (game->rayon->angle > M_PI / 2 && game->rayon->angle < 3 * M_PI / 2)
-		step = -step;
+	if ((game->rayon->angle > 0 && game->rayon->angle < M_PI) && step < 0)
+		return (-step);
+	if (!(game->rayon->angle > 0 && game->rayon->angle < M_PI) && step > 0)
+		return (-step);
 	return (step);
 }
 
@@ -28,9 +30,9 @@ float	step_x_y(t_game *game)
 	float	step;
 
 	step = TILE_SIZE;
-	if (!(game->rayon->angle > 0 && game->rayon->angle < M_PI))
-		step = -step;
-	return (step);
+	if (game->rayon->angle > 0 && game->rayon->angle < M_PI)
+		return (step);
+	return (-step);
 }
 
 // Y_AXIS
@@ -39,9 +41,9 @@ float	step_y_x(t_game *game)
 	float	step;
 
 	step = TILE_SIZE;
-	if (game->rayon->angle > M_PI / 2 && game->rayon->angle < 3 * M_PI / 2)
-		step = -step;
-	return (step);
+	if (!(game->rayon->angle > M_PI / 2 && game->rayon->angle < 3 * M_PI / 2))
+		return (step);
+	return (-step);
 }
 
 float	step_y_y(t_game *game)
@@ -49,7 +51,12 @@ float	step_y_y(t_game *game)
 	float step;
 
 	step = TILE_SIZE * tan(game->rayon->angle);
-	if (!(game->rayon->angle > 0 && game->rayon->angle < M_PI))
-		step = -step;
+
+	if ((game->rayon->angle > (M_PI / 2) && game->rayon->angle < (3 * M_PI) / 2)
+		&& step < 0)
+		return (-step);
+	if (!(game->rayon->angle > (M_PI / 2) && game->rayon->angle < (3 * M_PI)
+			/ 2) && step > 0)
+		return (-step);
 	return (step);
 }
