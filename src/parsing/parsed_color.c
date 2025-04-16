e70665d8c58e675a36f284b8f34e3863d0d5fec1
Jonathan Albiser <jalbiser@k1r4p15.42mulhouse.fr>
2025-04-16 21:11:07 +0200

    fix all

--- src/parsing/parsed_color.c
+++ src/parsing/parsed_color.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parsed_color.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jalbiser <jalbiser@student.42.fr>          #+#  +:+       +#+        */
+/*   By: jalbiser <jalbiser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025-01-23 22:27:51 by jalbiser          #+#    #+#             */
-/*   Updated: 2025-01-23 22:27:51 by jalbiser         ###   ########.fr       */
+/*   Created: 2025/01/23 22:27:51 by jalbiser          #+#    #+#             */
+/*   Updated: 2025/04/16 21:10:02 by jalbiser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,10 +29,10 @@ int	parsed_color(int **target, char *value)
 	{
 		trim(values[i]);
 		if (!ft_str_is_numeric(values[i]))
-			return (free(*target), free_split(values), 0);
+			return (free_split(values), 0);
 		val = ft_atoi(values[i]);
 		if (val < 0 || val > 255)
-			return (free(*target), free_split(values), 0);
+			return (free_split(values), 0);
 		(*target)[i] = val;
 		i++;
 	}
