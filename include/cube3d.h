00c9028d48917059f5a35edd32d9a1b0ba376779
Saliinger <alexisnoukan@gmail.com>
2025-04-19 18:45:54 +0200

    norm

--- include/cube3d.h
+++ include/cube3d.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cube3d.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jalbiser <jalbiser@student.42.fr>          +#+  +:+       +#+        */
+/*   By: anoukan <anoukan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 21:08:33 by anoukan           #+#    #+#             */
-/*   Updated: 2025/02/21 14:47:42 by jalbiser         ###   ########.fr       */
+/*   Updated: 2025/04/19 18:45:46 by anoukan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,6 @@
 
 # include "exec.h"
 # include "parsing.h"
-
-
 
 void	ft_error(char *str);
 
